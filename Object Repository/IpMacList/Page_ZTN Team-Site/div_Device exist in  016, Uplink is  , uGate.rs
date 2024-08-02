30a37e7eeb03b8965c3557544dc8ec608fc17ad5
chan <15575185236@163.com>
2024-08-02 09:26:50 +0800

    bug fix

--- Object Repository/IpMacList/Page_ZTN Team-Site/div_Device exist in  016, Uplink is  , uGate.rs
+++ Object Repository/IpMacList/Page_ZTN Team-Site/div_Device exist in  016, Uplink is  , uGate.rs
@@ -6,12 +6,11 @@
    <elementGuidId>3c50c930-3c5f-4ae6-a70c-0d3df49cf49f</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//div[@id='location-table']/div/div[3]/div/div/div/table/tbody/tr[9]/td[2]/div</value>
+         <key>CSS</key>
       </entry>
       <entry>
-         <key>CSS</key>
-         <value></value>
+         <key>XPATH</key>
+         <value>//div[@id='location-table']/div/div[3]/div/div/div/table/tbody/tr[9]/td[2]/div</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -29,7 +28,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>div</value>
-      <webElementGuid>88db8cd1-89df-4570-94df-c0a10ce36364</webElementGuid>
+      <webElementGuid>b3414a5f-751e-492e-a6b8-804f9255990a</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -37,7 +36,7 @@
       <name>class</name>
       <type>Main</type>
       <value>cell</value>
-      <webElementGuid>fb1273bf-5774-4b7e-8c89-6cb2f0b9cec8</webElementGuid>
+      <webElementGuid>fcf3e65e-d284-4057-b567-4844ad3f34f7</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -45,7 +44,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Device exist in  016, Uplink is  , uGate:  </value>
-      <webElementGuid>2438859e-0fc5-46dd-8a05-f4f06c4d7266</webElementGuid>
+      <webElementGuid>4b71a480-6fba-4e60-94a2-b4914180fdda</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -53,7 +52,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;location-table&quot;)/div[@class=&quot;el-table__inner-wrapper&quot;]/div[@class=&quot;el-table__body-wrapper&quot;]/div[@class=&quot;el-scrollbar&quot;]/div[@class=&quot;el-scrollbar__wrap el-scrollbar__wrap--hidden-default&quot;]/div[@class=&quot;el-scrollbar__view&quot;]/table[@class=&quot;el-table__body&quot;]/tbody[1]/tr[@class=&quot;el-table__row&quot;]/td[@class=&quot;el-table_3_column_14 el-table__cell&quot;]/div[@class=&quot;cell&quot;]</value>
-      <webElementGuid>d907e078-bbea-4631-8911-35ab0601278e</webElementGuid>
+      <webElementGuid>94c80195-99d8-4211-a2d1-8ff17aedc109</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -61,7 +60,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//div[@id='location-table']/div/div[3]/div/div/div/table/tbody/tr[9]/td[2]/div</value>
-      <webElementGuid>5352d1e4-fb07-4fdf-9346-b96be2a8833d</webElementGuid>
+      <webElementGuid>bde30c8b-7d5a-4e7d-99b4-857d5ffcd782</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -69,7 +68,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='test-001'])[1]/following::div[1]</value>
-      <webElementGuid>54823026-f722-48bb-9c25-eb2f80902662</webElementGuid>
+      <webElementGuid>29be23c6-3cb3-4f35-96a6-38b6c06c9acb</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -77,7 +76,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='a2554d83-aa0b-4e8b-8a23-50e90d02c191-1720493938533-003070'])[1]/following::div[5]</value>
-      <webElementGuid>3dd36a6a-9969-488a-9416-9769a95bf704</webElementGuid>
+      <webElementGuid>05ae0d24-45b6-4d49-abe0-8382805adf57</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -85,7 +84,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='dqwdqwdqwd123'])[1]/preceding::div[5]</value>
-      <webElementGuid>d553b60c-3765-4400-bfd0-f50faa761e4b</webElementGuid>
+      <webElementGuid>a83e2572-05a4-4b7e-8e7a-47d7a0959ce0</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -93,7 +92,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//tr[9]/td[2]/div</value>
-      <webElementGuid>0fe66548-ba03-4cac-ba78-09746ac0391e</webElementGuid>
+      <webElementGuid>828d92ae-4e95-41a9-a154-0c879c6d5216</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -101,6 +100,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//div[(text() = 'Device exist in  016, Uplink is  , uGate:  ' or . = 'Device exist in  016, Uplink is  , uGate:  ')]</value>
-      <webElementGuid>5624770e-1b5b-47b2-90c0-da9c7b46817d</webElementGuid>
+      <webElementGuid>fa41f8d3-b4e9-4a49-8489-0612da4eb574</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
